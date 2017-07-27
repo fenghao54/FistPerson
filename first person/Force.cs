d8c1c17d0108521c98da5d1c48edc14b2415ea7d
fenghao54 <2275704338>
2017-07-27 17:50:11 +0800

    first person C#

---
+++ first person/Force.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Force : MonoBehaviour {
+
+	
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+        Vector3 positon = transform.position;
+        positon.y = 0;
+        transform.position = positon;
+	}
+}
